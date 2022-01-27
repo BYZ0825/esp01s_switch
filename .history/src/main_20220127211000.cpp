804b33ef8e57fa1e3b5712a07c39a134d7e85285
BYZ0825 <byz0825@outlook.com>
2022-01-28 01:08:22 +0800

    完成测试

---
+++ .history/src/main_20220127211000.cpp
@@ -0,0 +1,171 @@
+/* *****************************************************************
+ *
+ * Download latest Blinker library here:
+ * https://github.com/blinker-iot/blinker-library/archive/master.zip
+ * 
+ * 
+ * Blinker is a cross-hardware, cross-platform solution for the IoT. 
+ * It provides APP, device and server support, 
+ * and uses public cloud services for data transmission and storage.
+ * It can be used in smart home, data monitoring and other fields 
+ * to help users build Internet of Things projects better and faster.
+ * 
+ * Make sure installed 2.7.4 or later ESP8266/Arduino package,
+ * if use ESP8266 with Blinker.
+ * https://github.com/esp8266/Arduino/releases
+ * 
+ * Make sure installed 1.0.5 or later ESP32/Arduino package,
+ * if use ESP32 with Blinker.
+ * https://github.com/espressif/arduino-esp32/releases
+ * 
+ * Docs: https://diandeng.tech/doc
+ *       
+ * 
+ * *****************************************************************
+ * 
+ * Blinker 库下载地址:
+ * https://github.com/blinker-iot/blinker-library/archive/master.zip
+ * 
+ * Blinker 是一套跨硬件、跨平台的物联网解决方案，提供APP端、设备端、
+ * 服务器端支持，使用公有云服务进行数据传输存储。可用于智能家居、
+ * 数据监测等领域，可以帮助用户更好更快地搭建物联网项目。
+ * 
+ * 如果使用 ESP8266 接入 Blinker,
+ * 请确保安装了 2.7.4 或更新的 ESP8266/Arduino 支持包。
+ * https://github.com/esp8266/Arduino/releases
+ * 
+ * 如果使用 ESP32 接入 Blinker,
+ * 请确保安装了 1.0.5 或更新的 ESP32/Arduino 支持包。
+ * https://github.com/espressif/arduino-esp32/releases
+ * 
+ * 文档: https://diandeng.tech/doc
+ *       
+ * 
+ * *****************************************************************/
+
+#define BLINKER_WIFI
+#define BLINKER_MIOT_MULTI_OUTLET
+#define BLINKER_WITHOUT_SSL //非SSL加密通信接入，省堆栈
+
+#include <Blinker.h>
+
+char auth[] = "1572b2d4cafd";
+char ssid[] = "HomeSlow";
+char pswd[] = "kxc1220a1b2c3";
+#define RELAYGPIO0 0 //GPIO 0
+#define RELAYGPIO2 2 //GPIO 2
+#define RELAYGPIO1 1 //TX GPIO1
+#define RELAYGPIO3 3 //RX GPIO3
+
+uint8_t SW0 = RELAYGPIO0;
+uint8_t SW1 = RELAYGPIO1;
+uint8_t SW2 = RELAYGPIO2;
+uint8_t SW3 = RELAYGPIO3;
+
+BlinkerButton Button0("btn-0"); //注意：要和APP组件’数据键名’一致
+BlinkerButton Button1("btn-1"); //注意：要和APP组件’数据键名’一致
+BlinkerButton Button2("btn-2"); //注意：要和APP组件’数据键名’一致
+BlinkerButton Button3("btn-3"); //注意：要和APP组件’数据键名’一致
+
+bool wsState[5] = {false};
+
+// 按下BlinkerAPP按键即会执行该函数
+void button0_callback(const String &state)
+{
+    BLINKER_LOG("get button0 state: ", state);
+    digitalWrite(SW0, !digitalRead(SW0));
+    Blinker.vibrate();
+}
+void button1_callback(const String &state)
+{
+    BLINKER_LOG("get button1 state: ", state);
+    digitalWrite(SW1, !digitalRead(SW1));
+    Blinker.vibrate();
+}
+void button2_callback(const String &state)
+{
+    BLINKER_LOG("get button2 state: ", state);
+    digitalWrite(SW2, !digitalRead(SW2));
+    Blinker.vibrate();
+}
+void button3_callback(const String &state)
+{
+    BLINKER_LOG("get button3 state: ", state);
+    digitalWrite(SW3, !digitalRead(SW3));
+    Blinker.vibrate();
+}
+
+//小爱电源类操作的回调函数:
+//当小爱同学向设备发起控制, 设备端需要有对应控制处理函数
+void miotPowerState(const String &state, uint8_t num)
+{
+    BLINKER_LOG("need set power state: ", state, "num_back", num);
+
+    if (state == BLINKER_CMD_ON)
+    {
+        digitalWrite(LED, LOW); //低电平点灯
+        BlinkerMIOT.powerState("on");
+        BlinkerMIOT.print();
+        wsState = true;
+    }
+    else if (state == BLINKER_CMD_OFF)
+    {
+        digitalWrite(LED, HIGH); //高电平开灯
+        BlinkerMIOT.powerState("off");
+        BlinkerMIOT.print();
+        wsState = false;
+    }
+}
+
+//小爱同学 的回调查询函数，照抄即可。主要是查询 当前灯的状态
+void miotQuery(int32_t queryCode, uint8_t num)
+{
+    BLINKER_LOG("MIOT Query codes: ", queryCode);
+    switch (queryCode)
+    {
+    case BLINKER_CMD_QUERY_ALL_NUMBER:
+        BLINKER_LOG("MIOT Query All");
+        BlinkerMIOT.powerState(wsState ? "on" : "off");
+        BlinkerMIOT.color(0);
+        BlinkerMIOT.mode(0);
+        BlinkerMIOT.colorTemp(1000);
+        BlinkerMIOT.brightness(1);
+        BlinkerMIOT.print();
+        break;
+    case BLINKER_CMD_QUERY_POWERSTATE_NUMBER:
+        BLINKER_LOG("MIOT Query Power State");
+        BlinkerMIOT.powerState(wsState ? "on" : "off");
+        BlinkerMIOT.print();
+        break;
+    default:
+        BlinkerMIOT.powerState(wsState ? "on" : "off");
+        BlinkerMIOT.color(0);
+        BlinkerMIOT.mode(0);
+        BlinkerMIOT.colorTemp(1000);
+        BlinkerMIOT.brightness(1);
+        BlinkerMIOT.print();
+        break;
+    }
+}
+
+void setup()
+{
+    // 初始化串口，并开启调试信息，调试用可以删除
+    Serial.begin(115200);
+    BLINKER_DEBUG.stream(Serial);
+    // 初始化IO
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, HIGH); // 初始化 led 高电平 ，则灯熄灭状态
+
+    // 初始化blinker
+    Blinker.begin(auth, ssid, pswd);
+    Button1.attach(button1_callback);
+
+    //小爱同学务必在回调函数中反馈该控制状态
+    BlinkerMIOT.attachPowerState(miotPowerState); //注册回调函数
+    BlinkerMIOT.attachQuery(miotQuery);
+}
+void loop()
+{
+    Blinker.run();
+}
---
+++ .history/src/main_20220128005041.cpp
@@ -0,0 +1,218 @@
+/* *****************************************************************
+ *
+ * Download latest Blinker library here:
+ * https://github.com/blinker-iot/blinker-library/archive/master.zip
+ * 
+ * 
+ * Blinker is a cross-hardware, cross-platform solution for the IoT. 
+ * It provides APP, device and server support, 
+ * and uses public cloud services for data transmission and storage.
+ * It can be used in smart home, data monitoring and other fields 
+ * to help users build Internet of Things projects better and faster.
+ * 
+ * Make sure installed 2.7.4 or later ESP8266/Arduino package,
+ * if use ESP8266 with Blinker.
+ * https://github.com/esp8266/Arduino/releases
+ * 
+ * Make sure installed 1.0.5 or later ESP32/Arduino package,
+ * if use ESP32 with Blinker.
+ * https://github.com/espressif/arduino-esp32/releases
+ * 
+ * Docs: https://diandeng.tech/doc
+ *       
+ * 
+ * *****************************************************************
+ * 
+ * Blinker 库下载地址:
+ * https://github.com/blinker-iot/blinker-library/archive/master.zip
+ * 
+ * Blinker 是一套跨硬件、跨平台的物联网解决方案，提供APP端、设备端、
+ * 服务器端支持，使用公有云服务进行数据传输存储。可用于智能家居、
+ * 数据监测等领域，可以帮助用户更好更快地搭建物联网项目。
+ * 
+ * 如果使用 ESP8266 接入 Blinker,
+ * 请确保安装了 2.7.4 或更新的 ESP8266/Arduino 支持包。
+ * https://github.com/esp8266/Arduino/releases
+ * 
+ * 如果使用 ESP32 接入 Blinker,
+ * 请确保安装了 1.0.5 或更新的 ESP32/Arduino 支持包。
+ * https://github.com/espressif/arduino-esp32/releases
+ * 
+ * 文档: https://diandeng.tech/doc
+ *       
+ * 
+ * *****************************************************************/
+
+#define BLINKER_WIFI
+#define BLINKER_MIOT_MULTI_OUTLET
+
+#include <Blinker.h>
+
+char auth[] = "1572b2d4cafd";
+char ssid[] = "HomeSlow";
+char pswd[] = "kxc1220a1b2c3";
+
+#define SW1 0; //gpio 0
+#define SW2 1; //gpio1  Tx
+#define SW3 2; //gpio2
+#define SW4 3; //gpio3 Rx
+
+uint8_t sw1 = SW1;
+uint8_t sw2 = SW2;
+uint8_t sw3 = SW3;
+uint8_t sw4 = SW4;
+
+// BlinkerButton Button4("sw4");
+// BlinkerButton Button1("sw1");
+// BlinkerButton Button2("sw2");
+// BlinkerButton Button3("sw3");
+
+bool oState[5] = {false};
+// 按下按键即会执行该函数
+void button0_callback(const String &state)
+{
+    BLINKER_LOG("get button state: ", state);
+    digitalWrite(sw0, !digitalRead(sw0));
+    Blinker.vibrate();
+    oState[sw0] = !oState[sw0];
+}
+void button1_callback(const String &state)
+{
+    BLINKER_LOG("get button state: ", state);
+    digitalWrite(sw1, !digitalRead(sw1));
+    Blinker.vibrate();
+    oState[sw1] = !oState[sw1];
+}
+
+void button2_callback(const String &state)
+{
+    BLINKER_LOG("get button state: ", state);
+    digitalWrite(sw2, !digitalRead(sw2));
+    Blinker.vibrate();
+    oState[sw2] = !oState[sw2];
+}
+void button3_callback(const String &state)
+{
+    BLINKER_LOG("get button state: ", state);
+    digitalWrite(sw3, !digitalRead(sw3));
+    Blinker.vibrate();
+    oState[sw3] = !oState[sw3];
+}
+
+void miotPowerState(const String &state, uint8_t num)
+{
+    BLINKER_LOG("need set outlet: ", num, ", power state: ", state);
+
+    if (state == BLINKER_CMD_ON)
+    {
+        if (num == 1)
+        {
+            digitalWrite(sw1, HIGH);
+        }
+        else if (num == 2)
+        {
+            digitalWrite(sw2, HIGH);
+        }
+        else if (num == 3)
+        {
+            digitalWrite(sw3, HIGH);
+        }
+        else if (num == 4)
+        {
+            digitalWrite(sw4, HIGH);
+        }
+        BlinkerMIOT.powerState("on", num);
+        BlinkerMIOT.print();
+        oState[num] = true;
+    }
+    else if (state == BLINKER_CMD_OFF)
+    {
+        if (num == 1)
+        {
+            digitalWrite(sw1, LOW);
+        }
+        else if (num == 2)
+        {
+            digitalWrite(sw2, LOW);
+        }
+        else if (num == 3)
+        {
+            digitalWrite(sw3, LOW);
+        }
+        else if (num == 4)
+        {
+            digitalWrite(sw4, LOW);
+        }
+        BlinkerMIOT.powerState("off", num);
+        BlinkerMIOT.print();
+        oState[num] = false;
+    }
+}
+
+void miotQuery(int32_t queryCode, uint8_t num)
+{
+    BLINKER_LOG("MIOT Query outlet: ", num, ", codes: ", queryCode);
+
+    switch (queryCode)
+    {
+    case BLINKER_CMD_QUERY_ALL_NUMBER:
+        BLINKER_LOG("MIOT Query All");
+        BlinkerMIOT.powerState(oState[num] ? "on" : "off", num);
+        BlinkerMIOT.print();
+        break;
+    case BLINKER_CMD_QUERY_POWERSTATE_NUMBER:
+        BLINKER_LOG("MIOT Query Power State");
+        BlinkerMIOT.powerState(oState[num] ? "on" : "off", num);
+        BlinkerMIOT.print();
+        break;
+    default:
+        BlinkerMIOT.powerState(oState[num] ? "on" : "off", num);
+        BlinkerMIOT.print();
+        break;
+    }
+}
+
+void dataRead(const String &data)
+{
+    BLINKER_LOG("Blinker readString: ", data);
+
+    Blinker.vibrate();
+
+    uint32_t BlinkerTime = millis();
+
+    Blinker.print("millis", BlinkerTime);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    // BLINKER_DEBUG.stream(Serial)
+
+    pinMode(sw1, OUTPUT);
+    digitalWrite(sw1, LOW);
+
+    pinMode(sw2, OUTPUT);
+    digitalWrite(sw2, LOW);
+
+    pinMode(sw3, OUTPUT);
+    digitalWrite(sw3, LOW);
+
+    pinMode(sw4, OUTPUT);
+    digitalWrite(sw4, LOW);
+
+    Blinker.begin(auth, ssid, pswd);
+    Blinker.attachData(dataRead);
+
+    // Button0.attach(button0_callback);
+    // Button1.attach(button1_callback);
+    // Button2.attach(button2_callback);
+    // Button3.attach(button3_callback);
+
+    BlinkerMIOT.attachPowerState(miotPowerState);
+    BlinkerMIOT.attachQuery(miotQuery);
+}
+
+void loop()
+{
+    Blinker.run();
+}
